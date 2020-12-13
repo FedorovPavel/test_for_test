d146af4cc5492c78767fe6e1ec97a45515132be8
Pavel <catkiller95@gmail.com>
2020-12-13 20:56:17 +0300

    CI

---
+++ config/config.js
@@ -0,0 +1,25 @@
+const env = process.env.NODE_ENV || "dev";
+
+const config = {
+    "test": {
+        port: 3000,
+        serverAddress: 'http://localhost:3000'
+    },
+    "dev": {
+        port: 3000,
+        serverAddress: 'http://localhost:3000'
+    },
+    "production": {
+        port: process.env.PORT || 15565
+    },
+    "prod": {
+        port: process.env.PORT || 15565,
+        serverAddress: 'https://testdev2088.herokuapp.com'
+    },
+    "ci": {
+        port: process.env.PORT || 15565,
+        serverAddress: 'https://testdev2088.herokuapp.com'
+    }
+}
+
+module.exports = config[env];
---
+++ test/test.test.js
@@ -0,0 +1,22 @@
+require('./../index');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const config = require('./../config/config');
+const serverAddress = config.serverAddress;
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+
+describe("Test", function () {
+    it("True is True", function (done) {
+        chai.request(serverAddress)
+            .get('/')
+            .then(function (response) {
+                expect(response).has.status(200);
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            })
+    });
+});
