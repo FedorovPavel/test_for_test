f90c07cd8d199f36707da49c340e181c19ab15e5
Pavel <catkiller95@gmail.com>
2020-12-13 22:17:40 +0300

    TEST?

--- test/test.test.js
+++ test/test.test.js
@@ -1,23 +1,22 @@
-// const chai = require('chai');
+const chai = require('chai');
 const mocha = require('mocha');
-// const chaiHttp = require('chai-http');
+const chaiHttp = require('chai-http');
 const config = require('./../config/config');
 const serverAddress = config.serverAddress;
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// const expect = chai.expect;
+const expect = chai.expect;
 
 describe("Test", function () {
     it("True is True", function (done) {
-        done();
-        // chai.request(serverAddress)
-        //     .get('/')
-        //     .then(function (response) {
-        //         expect(response).has.status(200);
-        //         done();
-        //     })
-        //     .catch(function (err) {
-        //         done(err);
-        //     })
+        chai.request(serverAddress)
+            .get('/')
+            .then(function (response) {
+                expect(response).has.status(200);
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            })
     });
 });
